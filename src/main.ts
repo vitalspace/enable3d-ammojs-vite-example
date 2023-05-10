eeeb447c98458a9e88de82cbaea73edd64184c85
vitalspace <myglobalsounds@gmail.com>
2023-05-09 18:45:58 -0600

    Updated project

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import "./scene";
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div id="info-text">Enable3d Ammojs Vite Example.<br />Works on mobile and desktop.</div>
+`;
